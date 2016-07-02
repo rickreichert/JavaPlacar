2bacc6e593b812b6b761eea69ca11603219c3ea6
Ricardo Reichert <rickreichert@hotmail.com>
2016-07-02 18:13:59 -0300

    Metodos staticos
    Oloko Bixo!!

--- Inicial.java
+++ Inicial.java
@@ -6,29 +6,33 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import classes.UTIL.Equipe;
 
 @SuppressWarnings("serial")
-public class Inicial extends JFrame {
+public class Inicial extends JFrame{
 	
 	private AcaoMenu action = new AcaoMenu();
 	
 	private Container contPanel;
 	private Container contMenu;
 	
+	private JPanel jpAjuda = new JPanel();
 	private JPanel jpCadas = new PanelCadas();
 	private JPanel jpPlacar = new JPanel();
 	
 	private JButton jCad = new JButton("Equipes");
 	private JButton jExit = new JButton("Sair");
 	private JButton jPart = new JButton("Partida");
+	private JButton jAjuda = new JButton("Ajuda");
 	
-	private Equipe listaEquipe = new Equipe();
+	private static ArrayList<Equipe> listaEquipe = new ArrayList<Equipe>();
 	
 	
 	public static void main(String[] args) {
@@ -40,21 +44,24 @@ public class Inicial extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
 		this.setTitle("MENU");
-		this.setBounds(500, 100, 800, 500);
+		this.setSize(800, 500);
+		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		this.getContentPane().setLayout(new BorderLayout());
 		
 		contMenu = new JPanel();
-		contMenu.setLayout(new GridLayout(1, 3));
+		contMenu.setLayout(new GridLayout(1, 4));
 
 		contPanel = new JPanel();
 		contPanel.setLayout(new CardLayout());
 		
-		contPanel.add(jpCadas, 0);
-		contPanel.add(jpPlacar, 1);
+		contPanel.add(jpAjuda, 0);
+		contPanel.add(jpCadas, 1);
+		contPanel.add(jpPlacar, 2);
 		
 		contMenu.add(jCad);
 		contMenu.add(jPart);
+		contMenu.add(jAjuda);
 		contMenu.add(jExit);
 				
 		this.getContentPane().add(contMenu, BorderLayout.NORTH);
@@ -63,6 +70,7 @@ public class Inicial extends JFrame {
 		jCad.addActionListener(action);
 		jPart.addActionListener(action);
 		jExit.addActionListener(action);
+		jAjuda.addActionListener(action);
 		
 	}	
 	
@@ -71,17 +79,45 @@ public class Inicial extends JFrame {
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource() == jCad){
 				jpPlacar.setVisible(false);
+				jpAjuda.setVisible(false);
 				jpCadas.setVisible(true);
 			}
 			else
 				if(e.getSource() == jPart){
-				jpCadas.setVisible(false);
-				jpPlacar.setVisible(true);
-			}else
-				if(e.getSource() == jExit){
-			        System.exit(0);
-				}
+					if(listaEquipe.size() >= 2){
+						jpCadas.setVisible(false);
+						jpAjuda.setVisible(false);
+						jpPlacar.setVisible(true);	
+					}else{
+				        JOptionPane.showMessageDialog(null,"Você precisa Cadastrar DUAS Equipes antes de iniciar uma partida!!");
+					}
+					
+				}else
+					if(e.getSource() == jExit){
+				        System.exit(0);
+					}else
+						if(e.getSource() == jAjuda){
+							jpCadas.setVisible(false);
+							jpPlacar.setVisible(false);
+							jpAjuda.setVisible(true);
+						}
 		}
 		
 	}
+	
+	public static ArrayList<Equipe> getListEquipes(){
+		return listaEquipe;
+	}
+	
+	public static String[] retornaArray_com_Equipes(){
+		
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.add(" ");
+		for(int i = 0; i < listaEquipe.size(); i++){
+			lista.add(listaEquipe.get(i).getCapitaoEquip().getNome());
+		}
+		
+		return (String [])lista.toArray(new String [lista.size()]);
+	}
+
 }
